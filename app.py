a08caf36a0c9b4382484386fe1eb259ca03b2d04
Kurt. <76596317+kurtesallen@users.noreply.github.com>
2025-12-23 17:25:11 -0500

    Initialize AWS CDK app with AccessReviewStack

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-app.py
+#!/usr/bin/env python3
+import aws_cdk as cdk
+
+from iam_access_review.access_review_stack import AccessReviewStack
+
+
+app = cdk.App()
+
+AccessReviewStack(
+    app,
+    "IamAccessReviewStack",
+    # You can set env here if you want to pin to an account/region
+    # env=cdk.Environment(account="123456789012", region="us-east-1"),
+)
+
+app.synth()
